8412720b48ac3de6d262974ce55a25f06254f2ec
Michel Conrado <michelconrado@outlook.com>
2018-08-08 18:28:01 -0300

    continuing 2 the support for the K8s - not Fully
    - Add check "default versio"
    - Add cook_Storage for Badger flag
    - Add the option to opt for cooking docker or k8s
    - add render_template context to k8s
    - Add warning
    -

--- cook-a-bulk.sh
+++ cook-a-bulk.sh
@@ -6,10 +6,21 @@ echo "================================================"
 echo "================================================"
 
 
+set_vars_docker (){
 THIS_DIR=`dirname $0`
 source $THIS_DIR/service/set-of-vars.sh
+echo "Using vars for docker"
+echo "The version we are using is ==> $dgraphVersion"
+}
 
-echo "$dgraphVersion"
+set_vars_k8s (){
+THIS_DIR=`dirname $0`
+echo "Using vars for kubernetes"
+source $THIS_DIR/service-k8s/set-of-vars-k8s.sh
+
+echo "The version we are using is ==> $dgraphVersion"
+}
+
 
 render_template () {
   eval "echo \"$(cat $1)\""
@@ -17,12 +28,23 @@ render_template () {
 
 start_to_cook () {
 
+     checkDFHost () {
+     if [ !$addrHost ];
+     then
+     echo 'Theres no default addr'
+     addrHost=192.168.99.100
+     echo "Defining ${addrHost} as default addr"
+     else
+     echo "ure using ${addrHost} addr"
+     fi 
+     } 
+
     questione_Host () {
     while true; do
     read -p "Do you wish to change host addr? (it's important) " yn
     case $yn in
         [Yy]* ) return 0; break;;
-        [Nn]* ) return 1; break;;
+        [Nn]* ) checkDFHost; return 1; break;;
         * ) echo "Please answer yes or no.";;
     esac
 done
@@ -39,13 +61,23 @@ if questione_Host; then
 }
 
 cook_version () {
+ checkDF () {
+     if [ !$dgraphVersion ];
+     then
+     echo 'Theres no default version'
+     dgraphVersion='v1.0.7-rc4'
+     echo "Defining ${dgraphVersion} as default version"
+     else
+     echo "ure using ${dgraphVersion} version"
+     fi 
+     } 
 
     questione_Version () {
     while true; do
     read -p "Do you wish to change the Dgraph Version? " yn
     case $yn in
             [Yy]* ) return 0; break;;
-            [Nn]* ) return 1; break;;
+            [Nn]* ) checkDF; return 1; break;;
             * ) echo "Please answer yes or no.";;
         esac
     done
@@ -63,12 +95,22 @@ if questione_Version; then
 
 
 cook_Bind () {
+     checkDFBind () {
+     if [ !$bindall ];
+     then
+     echo 'Theres no default bindall'
+     bindall=true
+     echo "Defining ${bindall} as default bind"
+     else
+     echo "ure using ${bindall} as default bind"
+     fi 
+     } 
     questione_TheBind () {
     while true; do
     read -p "Do you wish to change the bindall default? " yn
     case $yn in
         [Yy]* ) return 0; break;;
-        [Nn]* ) return 1; break;;
+        [Nn]* ) checkDFBind; return 1; break;;
         * ) echo "Please answer yes or no.";;
     esac
 done
@@ -88,13 +130,72 @@ if questione_TheBind; then
      fi
 }
 
+cook_Storage () {
+     checkDFST () {
+     if [ !$StorageType ];
+     then
+     echo 'Theres no default Storage Type'
+     StorageType='ssd'
+     echo "Defining ${StorageType} as default Storage Type"
+     else
+     echo "ure using ${StorageType} as Storage Type"
+     fi 
+     } 
+    questione_The_StorageType () {
+    while true; do
+    read -p "Do you wish to change the Storage type? " yn
+    case $yn in
+        [Yy]* ) return 0; break;;
+        [Nn]* ) checkDFST; return 1; break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+}
+if questione_The_StorageType; then
+        Option1="- I'm using SSD"
+        Option2="- I'm using HDD (harddrive)"
+
+        PS3='Please enter your choice: '
+        options=("${Option1}" "$Option2")
+        select opt in "${options[@]}"
+        do
+            case $opt in
+                "${Option1}")
+                    echo "You defined: $StorageType as default"
+                    return 0
+                    break
+                    ;;
+                "$Option2")
+                    echo "You defined: $StorageType as default"
+                    return 1
+                    break
+                    ;;
+                *) echo "invalid option $REPLY";;
+            esac
+        done
+             else
+                return 0
+     fi
+
+}
+
 cook_Memory () {
+     checkDFMemo () {
+     if [ !$my_server_memory ];
+     then
+     echo 'Theres no default version'
+     my_server_memory=2048
+     echo "Defining ${my_server_memory} as default memory"
+     else
+     echo "ure using ${my_server_memory} of memory"
+     fi 
+     } 
     questione_Memory () {
     while true; do
     read -p "Do you wanna increase the memory? " yn
     case $yn in
         [Yy]* ) return 0; break;;
-        [Nn]* ) return 1; break;;
+        [Nn]* ) checkDFMemo; return 1; break;;
         * ) echo "Please answer yes or no.";;
     esac
 done
@@ -107,6 +208,36 @@ if questione_Memory; then
         else
         return 0
      fi
+}
+
+questione_it_type () {
+
+
+Option1="- Cook a Docker-compose"
+Option2="- Cook a kubernetes yaml"
+
+PS3='Please enter your choice: '
+options=("${Option1}" "$Option2" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "${Option1}")
+            echo "So, let's cook a Docker-compose"
+            return 0
+            break
+            ;;
+        "$Option2")
+            echo "So, let's cook a kubernetes yaml"
+            return 1
+            break
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
+
 }
 
 questione_it () {
@@ -123,7 +254,7 @@ done
 
  generate_docker_compose () {
   echo "#### Creating docker-compose ..."
-  render_template ./templates/docker-compose.yaml > ./docker-compose.yml
+  render_template ./templates/docker-compose.yaml > ./service/docker-compose.yml
 }
 
  generate_bulk_script () {
@@ -140,22 +271,93 @@ done
     echo "#### done!"
 }
 
+ generate_k8s_yaml_bulk () {
+  echo "#### Creating dgraph-ha-with-bulk.yaml ..."
+  render_template ./templates/dgraph-ha-with-bulk.yaml > ./service-k8s/dgraph-ha-with-bulkTESTE.yaml
+}
 
- if questione_it; then
-    start_to_cook
-    cook_version
-    cook_Bind
-    cook_Memory
-    generate_docker_compose
-    generate_bulk_script
- echo "
-  new Dgraph Version ${dgraphVersion}
-  new Addr Host ${addrHost}
-  new Bindall ${bindall}
-  New Zero Port ${zeroPort}
-  New Memory value ${my_server_memory}"
- echo "#### Everything done! go compose it up!"
-    else
-        exit
+gen_scrt(){
+    cat << _EOF_ 
+'[[ \`hostname\` =~ -([0-9]+)$ ]] || exit 1 
+              ordinal=\${BASH_REMATCH[1]} 
+              idx=\$((\$ordinal + 1)) 
+              if [[ \$ordinal -eq 0 ]]; then '
+
+_EOF_
+}
+
+# back-up
+# '\"[[ \`hostname\` =~ -([0-9]+)$ ]] || exit 1 \\${newLine}n
+# ordinal=\${BASH_REMATCH[1]} \\${newLine}n
+# idx=\$(($ordinal + 1)) \\${newLine}n
+# if [[ $ordinal -eq 0 ]]; then \\${newLine}n \"'
+
+ generate_k8s_bulk_script () {
+  echo "#### Creating a NEW set-of-vars-k8s.sh ..."
+  newDgraphVersion=${dgraphVersion}
+  a='$'
+  b='idx'
+  newIdxk8s=\'${a}${b}\'
+  newAddrHost=${addrHost}
+  newBindall=${bindall}
+  NewZeroPort=${zeroPort}
+  SType=${StorageType}
+  newMy_server_memory=${my_server_memory}
+  NewclipCommandServerLine5=$(gen_scrt)
+  Thisa=''
+  Thisb='"-c"'
+  AWAITCMD=\'${Thisa}${Thisb}\'
+  curling='curl -LJO '${DfcurlAdd}' && echo downloaded && sh ./test.sh &&'
+  curling2=
+  NewcurlCommand=\'${curling}\'
+#    gen_scrt > NewclipCommandServerLine5
+  render_template ./templates/set-of-vars-k8s.tmpl > ./service-k8s/set-of-vars-k8s.sh
+    echo "#### done!"
+}
+
+warning() {
+    echo "################################"
+    echo "Attention: If for some reason the files don't update"
+    echo "Please, redo this process"
+    echo "################################"
+}
+
+
+ if questione_it_type; then
+        set_vars_docker
+        questione_it
+        start_to_cook
+        cook_version
+        cook_Bind
+        cook_Memory
+        echo "
+        new Dgraph Version ${dgraphVersion}
+        new Addr Host ${addrHost}
+        new Bindall ${bindall}
+        New Zero Port ${zeroPort}
+        New Memory value ${my_server_memory}"
+        generate_docker_compose
+        generate_bulk_script
+        echo "#### Everything done! go compose it up!"
+     else
+        if questione_it; then
+                set_vars_k8s
+                cook_version
+                cook_Storage
+                cook_Bind
+                cook_Memory
+                echo "
+                new Dgraph Version ${dgraphVersion}
+                new Storage Type ${StorageType}
+                new Bindall ${bindall}
+                New Zero Port ${zeroPort}
+                New Memory value ${my_server_memory}"
+                generate_k8s_yaml_bulk
+                generate_k8s_bulk_script
+                echo "#### Everything done! go run k8s-up.sh!"
+    fi
+    warning
+    exit
   fi
+  warning
 exit
---
+++ service-k8s/set-of-vars-k8s.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+dgraphVersion=v1.0.7-rc4
+addrHost=
+Rep0=3
+zeroPortDF=5080
+ServerReplicasDF=6
+DfcurlAdd='https://raw.githubusercontent.com/MichelDiz/test/master/test.sh'
+
+#Zero Configs
+bindall=true
+zeroPort=5080
+zeroReplicas=3
+idxk8s='$idx'
+serviceName='dgraph-zero'
+#Server Configs
+ServerReplicas=6
+my_server_memory=2048
+StorageType=ssd
+
+curlCommand='curl -LJO https://raw.githubusercontent.com/MichelDiz/test/master/test.sh && echo downloaded && sh ./test.sh &&'
+
+#safe command templating
+clipCommandServerLine2='"-c"'
+pipeCmd='|'
+clipCommandServerLine5='[[ `hostname` =~ -([0-9]+)$ ]] || exit 1 
+              ordinal=${BASH_REMATCH[1]} 
+              idx=$(($ordinal + 1)) 
+              if [[ $ordinal -eq 0 ]]; then '
